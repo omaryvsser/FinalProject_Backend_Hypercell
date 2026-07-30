db076676d247bfb78cb4982a1ff001c0323bca1d
SERAJ316 <serajahmed260@gmail.com>
2026-07-30 18:02:10 +0300

    Initial commit - Spring Boot setup

---
+++ event-ticketing-platform/event-ticketing-platform/src/test/java/com/hypercell/event_ticketing_platform/EventTicketingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hypercell.event_ticketing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
